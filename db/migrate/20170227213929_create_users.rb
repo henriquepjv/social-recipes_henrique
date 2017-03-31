d7e89bf3b71d196ff41f64f737f0b460bb54efd2
henrique <henriquepjv@hotmail.com>
2017-03-31 19:13:12 -0300

    migrations fix

--- db/migrate/20170227213929_create_users.rb
+++ db/migrate/20170227213929_create_users.rb
@@ -2,8 +2,6 @@ class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :encrypted_password
-      t.string :email
       t.boolean :administrator
 
       t.timestamps
