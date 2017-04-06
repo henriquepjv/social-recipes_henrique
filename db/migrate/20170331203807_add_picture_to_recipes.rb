5da87df06e3c2d15c379a373a9639a018ad997bf
Junqueira <henriquepjv@hotmail.com>
2017-04-06 11:27:16 -0300

    docker working

--- db/migrate/20170331203807_add_picture_to_recipes.rb
+++
@@ -1,9 +0,0 @@
-class AddPictureToRecipes < ActiveRecord::Migration[5.0]
-  def up
-     add_attachment :recipes, :picture
-   end
-
-   def down
-     remove_attachment :recipes, :picture
-   end
- end
